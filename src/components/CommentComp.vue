e69c2a0adaac2ee8e20cb1e3d42fd8ea7f00973f
MithileshHinge <mithhinge@gmail.com>
2022-05-13 05:31:31 +0530

    commentService added; CommentComp made recursive complying to backend response format;

--- src/components/CommentComp.vue
+++ src/components/CommentComp.vue
@@ -1,53 +1,63 @@
 <template>
-	<b-media :class="comment.isReply && 'sq-comment-isreply'" class="mt-2 mr-2" no-body>
-		<b-media-aside class="mx-2">
-			<b-avatar src="@/assets/tushar.png" size="2rem"></b-avatar>
-		</b-media-aside>
-		<b-media-body>
-			<b-container class="sq-comment-container sq-text">
-				<b-row no-gutters>
-					<b-col class="sq-font-regular">
-						{{ comment.userfullName }}
-					</b-col>
-					<b-col cols="auto" class="sq-subtext">
-						{{ comment.timestamp }}
-					</b-col>
-				</b-row>
-				<b-row no-gutters>
-					<b-col>
-						{{ comment.comment }}
-					</b-col>
-				</b-row>
-				<b-row no-gutters class="mt-2">
-					<b-col cols="auto">
-						<b-icon-heart-fill v-if="liked" @click="toggleLikeComment"></b-icon-heart-fill>
-						<b-icon-heart v-else @click="toggleLikeComment"></b-icon-heart>
-					</b-col>
-					<b-col v-if="comment.likes > 1" cols="auto" class="ml-1 mr-3" @click="toggleLikeComment">
-						{{ comment.likes }} likes
-					</b-col>
-					<b-col v-else-if="comment.likes > 0" cols="auto" class="ml-1 mr-3" @click="toggleLikeComment">
-						{{ comment.likes }} like
-					</b-col>
-					<b-col v-else cols="auto" class="ml-1 mr-3" @click="toggleLikeComment">
-						Like
-					</b-col>
-					<b-col cols="auto">
-						<b-icon-reply></b-icon-reply>
-					</b-col>
-					<b-col cols="auto" class="ml-1 mr-3">
-						Reply
-					</b-col>
-				</b-row>
-			</b-container>
-		</b-media-body>
-	</b-media>
+	<div>
+		<b-media :class="isReply && 'sq-comment-isreply'" class="mt-2 mr-2" no-body>
+			<b-media-aside class="mx-2">
+				<b-avatar src="@/assets/tushar.png" size="2rem"></b-avatar>
+			</b-media-aside>
+			<b-media-body>
+				<b-container class="sq-comment-container sq-text">
+					<b-row no-gutters>
+						<b-col class="sq-font-regular">
+							Mithilesh Hinge
+						</b-col>
+						<b-col cols="auto" class="sq-subtext">
+							{{ '2h' }}
+						</b-col>
+					</b-row>
+					<b-row no-gutters>
+						<b-col>
+							{{ comment.text }}
+						</b-col>
+					</b-row>
+					<b-row no-gutters class="mt-2">
+						<b-col cols="auto">
+							<b-icon-heart-fill v-if="liked" @click="toggleLikeComment"></b-icon-heart-fill>
+							<b-icon-heart v-else @click="toggleLikeComment"></b-icon-heart>
+						</b-col>
+						<!--b-col v-if="comment.likes > 1" cols="auto" class="ml-1 mr-3" @click="toggleLikeComment">
+							{{ comment.likes }} likes
+						</b-col>
+						<b-col v-else-if="comment.likes > 0" cols="auto" class="ml-1 mr-3" @click="toggleLikeComment">
+							{{ comment.likes }} like
+						</b-col>
+						<b-col v-else cols="auto" class="ml-1 mr-3" @click="toggleLikeComment">
+							Like
+						</b-col-->
+						<b-col cols="auto" class="ml-1 mr-3" @click="toggleLikeComment">
+							Like
+						</b-col>
+						<b-col cols="auto">
+							<b-icon-reply></b-icon-reply>
+						</b-col>
+						<b-col cols="auto" class="ml-1 mr-3">
+							Reply
+						</b-col>
+					</b-row>
+				</b-container>
+			</b-media-body>
+		</b-media>
+		<div v-if="!isReply">
+			<CommentComp v-for="reply in comment.replies" :key="reply.commentId" :comment="reply" :isReply="true"/>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
+	name: 'CommentComp',
 	props: {
 		comment: Object,
+		isReply: Boolean,
 	},
 	data() {
 		return {
---
+++ src/services/comment.service.js
@@ -0,0 +1,17 @@
+import api from './api.service';
+import handleError from './errorHandler.service';
+
+export default {
+	addComment(postId, text) {
+		return api.post(`/comment/${postId}`, { text })
+			.catch((err) => handleError(err));
+	},
+	addReply(postId, commentId, text) {
+		return api.post(`/comment/${postId}`, { text, replyToCommentId: commentId })
+			.catch((err) => handleError(err));
+	},
+	getCommentsOnPost(postId) {
+		return api.get(`/comments/${postId}`)
+			.catch((err) => handleError(err));
+	},
+};
