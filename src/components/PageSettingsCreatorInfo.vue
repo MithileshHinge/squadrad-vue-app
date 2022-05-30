285a0a6d5da1d7456a9b6592d3e7471d5f109f52
MithileshHinge <mithhinge@gmail.com>
2022-05-31 04:00:55 +0530

    Last commit mistake fixed: Incorrect cols in creatorInfo submit btn;

--- src/components/PageSettingsCreatorInfo.vue
+++ src/components/PageSettingsCreatorInfo.vue
@@ -61,12 +61,12 @@
 				</b-form-group>
 			</b-card>
 			<ButtonSubmit class="d-lg-none" :isProcessing="isSaving" :isProcessed="isSaved"/>
-			<b-row no-gutters class="d-none d-lg-flex fixed-bottom">
-				<b-col cols="6"/>
-				<b-col cols="3" class="d-flex">
+			<b-row no-gutters class="d-none d-lg-flex fixed-bottom" align-h="center">
+				<b-col cols="3"/>
+				<b-col cols="4" class="d-flex">
 					<ButtonSubmit class="position-relative" :isProcessing="isSaving" :isProcessed="isSaved"/>
 				</b-col>
-				<b-col cols="3"/>
+				<b-col cols="1"/>
 			</b-row>
 			<!-- div to clear fixed submit button from occluding last card-->
 			<div style="height: 3.5rem;"/>
