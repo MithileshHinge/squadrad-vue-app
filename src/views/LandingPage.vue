ecc9fe98eda5e206246bda656c9e63383d34f4af
MithileshHinge <mithhinge@gmail.com>
2022-06-23 11:31:37 +0530

    LandingPage changed to "website under maintenance"

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,8 +1,18 @@
 <template>
-	<div>
-		<b-img src="@/assets/squadrad-fav.png" fluid left></b-img>
-		<router-view></router-view>
-	</div>
+	<b-row no-gutters align-h="center" align-v="center">
+		<b-col cols="12">
+			<b-row>
+				<b-col>
+					<b-img src="@/assets/buno3.png" fluid center></b-img>
+				</b-col>
+			</b-row>
+			<b-row>
+				<b-col class="sq-text text-center pb-5">
+					Website is under maintenance<br>We thank you for your patience
+				</b-col>
+			</b-row>
+		</b-col>
+	</b-row>
 </template>
 
 <script>
